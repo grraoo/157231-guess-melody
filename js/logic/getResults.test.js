62e98b16b7e8dc7ec7a589d33807b16ed5e80ee4
grraoo <evil@grraoo.ru>
2018-04-06 15:37:09 +0300

    getResults

--- js/logic/getResults.test.js
+++ js/logic/getResults.test.js
@@ -2,15 +2,15 @@ import {assert} from 'chai';
 import getResults from './getResults';
 let myresultsAll = [6, -1, 0, 5, 8, 5, 4, 7, 20];
 let myresultCurr = {
-  points: 15,
+  points: 1,
   attempts: 1,
-  timeLeft: 45,
+  timeLeft: 1,
 };
 
 describe(`Array`, () => {
   describe(`count total()`, () => {
-    it(`should return 2`, () => {
-      assert.equal(3, getResults(myresultsAll, myresultCurr));
+    it(`should return string ${getResults(myresultsAll, myresultCurr)}`, () => {
+      assert.equal(`string`, typeof getResults(myresultsAll, myresultCurr));
     });
   });
 });
