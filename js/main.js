2cd315da708c4f55407c07ecbfc3273b431b1630
Grraoo <evil@grraoo.ru>
2018-04-12 22:12:51 +0300

    header and 2-nd screen

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
 import rnd from "./utils/rnd";
 import switchScreen from "./utils/switch-screen";
 import welcomeScreen from "./screens/screen-main";
-import authorScreen from "./screens/screen-author";
+import authorScreenTemplate from "./screens/screen-author";
 import genreScreen from "./screens/screen-genre";
 import resultsWin from "./screens/screen-result-win";
 import resultsTime from "./screens/screen-result-time";
 import resultsErrors from "./screens/screen-result-errors";
 import gameState from "./logic/game";
-
+import getElementFromTemplate from "./utils/getElementFromTemplate";
 const resultScreens = [resultsWin, resultsTime, resultsErrors];
 
-/**
- * welcome -> author
- */
-const playBtn = welcomeScreen.querySelector(`.main-play`);
-playBtn.addEventListener(`click`, function () {
-  switchScreen(authorScreen);
-});
+const generateAuthorScreen = () => getElementFromTemplate(authorScreenTemplate());
+const answerBtn = genreScreen.querySelector(`.genre-answer-send`);
 
 /**
- * author -> genre
- */
-const answerAuthor = authorScreen.querySelector(`.main-list`);
-const answerBtn = genreScreen.querySelector(`.genre-answer-send`);
-answerAuthor.addEventListener(`change`, function () {
-  gameState.answerDone++;
-  gameState.answers.push({success: !!Math.round(Math.random() + 0.3), time: rnd.number(60)});
-  if (gameState.answerDone > 4) {
-    switchScreen(genreScreen);
-    answerAuthor.reset();
-    answerBtn.disabled = true;
-  } else {
-    switchScreen(authorScreen);
-  }
+* welcome -> author
+*/
+const playBtn = welcomeScreen.querySelector(`.main-play`);
+playBtn.addEventListener(`click`, function () {
+  getNewAuthorScreen();
 });
+
+const getNewAuthorScreen = () => {
+  const screenF = generateAuthorScreen();
+  switchScreen(screenF);
+
+  /**
+   * author -> genre
+   */
+  const answerAuthor = screenF.querySelector(`.main-list`);
+  answerAuthor.addEventListener(`change`, function () {
+    gameState.answerDone++;
+    gameState.answers.push({success: !!Math.round(Math.random() + 0.3), time: rnd.number(60)});
+    if (gameState.answerDone > 4) {
+      switchScreen(genreScreen);
+      answerAuthor.reset();
+      answerBtn.disabled = true;
+    } else {
+      getNewAuthorScreen();
+    }
+  });
+};
 
 /**
  * genre -> result
--- js/screens/screen-author.js
+++ js/screens/screen-author.js
@@ -1,30 +1,14 @@
-import getElementFromTemplate from "../utils/getElementFromTemplate";
 import melodies from "../data/melodies";
-import gameState from "../logic/game";
 import rnd from "../utils/rnd";
+import header from "./header";
 
-const songs = rnd.array(melodies, 3);
 
-const theSong = songs[rnd.number(songs.length)];
+const template = () => {
+  const songs = rnd.array(melodies, 3);
+  const theSong = songs[rnd.number(songs.length)];
 
-const template =
-`<section class="main main--level main--level-artist">
-<svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
-  <circle
-    cx="390" cy="390" r="370"
-    class="timer-line"
-    style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
-
-  <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-    <span class="timer-value-mins">05</span><!--
-    --><span class="timer-value-dots">:</span><!--
-    --><span class="timer-value-secs">00</span>
-  </div>
-</svg>
-<div class="main-mistakes">
-  ${new Array(gameState.notes).fill(`<img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">`).join(`\n\t`)}
-</div>
-
+  return `<section class="main main--level main--level-artist">
+  ${header()}
 <div class="main-wrap">
   <h2 class="title main-title">Кто исполняет эту песню?</h2>
   <div class="player-wrapper">
@@ -51,6 +35,7 @@ const template =
   </form>
 </div>
 </section>`;
-const screen = getElementFromTemplate(template);
+};
+// const screen = getElementFromTemplate(template());
 
-export default screen;
+export default template;
