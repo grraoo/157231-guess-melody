c92230ea7ad213e0d18c93e9b9a28d0e7e81c9d3
grraoo <evil@grraoo.ru>
2018-04-17 15:37:22 +0300

    initialState and reset

--- js/logic/game.js
+++ js/logic/game.js
@@ -4,37 +4,35 @@ const initialState = {
   notes: 3,
   time: 300,
   screen: `welcome`,
+  results: [0, 10, 20, 12, 16, 14, 15, 7, 5],
+  questionsArr: questions
 };
-const resultsArr = [0, 10, 20, 12, 16, 14, 15, 7, 5];
 
 class GameState {
-  constructor(state, results, questionsArr) {
+  constructor(state) {
+    this.initialState = state;
     this.answers = [];
     this.notes = state.notes;
     this.time = state.time;
     this.screen = state.screen;
-    this.initialState = state;
-    this.results = results;
-    this.questions = questionsArr;
+    this.results = state.results;
+    this._questions = state.questionsArr;
   }
   reset() {
     this.answers.length = 0;
     this.notes = this.initialState.notes;
     this.time = this.initialState.time;
     this.screen = this.initialState.screen;
+    this._questions = this.initialState.questionsArr;
   }
 
   get question() {
     return this.questions.next();
   }
-  set questions(questionsArr) {
-    this._questions = questionsArr;
-  }
 
   get questions() {
-    return this._questions;
+    return (this.time <= 0 || this.notes <= 0) ? null : this._questions;
   }
-
 }
 
-export default new GameState(initialState, resultsArr, questions);
+export default new GameState(initialState);
